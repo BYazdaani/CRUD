2f1dc7c0e90d587ecb74ad5a2bee26cc5eef204c
Behrooz <32264262+BYazdaani@users.noreply.github.com>
2022-07-24 00:57:21 +0430

    401-05-01

---
+++ update.php
@@ -0,0 +1,17 @@
+<?php
+<!DOCTYPE html>
+
+<html>
+<head>
+ <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+ <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+
+<title>List Interance</title>
+</head>
+<div class="row">
+    <center>list</center>
+</div>
+
+</html>
+?>
